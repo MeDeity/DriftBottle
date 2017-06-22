7b9a031ed7378e4eb509cc797c8451d6440d55a2
MeDeity <546024423@qq.com>
2017-06-22 23:11:22 +0800

    漂流瓶接收界面及发送界面

--- app/src/main/java/com/deity/driftbottle/MainActivity.java
+++ app/src/main/java/com/deity/driftbottle/MainActivity.java
@@ -1,13 +1,37 @@
 package com.deity.driftbottle;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.send_nav_image)
+    public ImageView send_nav_image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @SuppressWarnings("unused")
+    @OnClick(R.id.send_nav_image)
+    public void sendBottleMsg(View view){
+        new AlertDialog.Builder(MainActivity.this).setView(R.layout.dialog_bottle_send)
+                .show();
+    }
+
+    @SuppressWarnings("unused")
+    @OnClick(R.id.get_nav_image)
+    public void getBottleMsg(View view){
+        new AlertDialog.Builder(MainActivity.this).setView(R.layout.dialog_bottle_receiver)
+                .show();
     }
 }
