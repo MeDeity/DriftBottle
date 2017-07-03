90149d327abbb616d76a754b1af9c8181624fdb5
MeDeity <546024423@qq.com>
2017-07-03 22:59:52 +0800

    微信第三方登录集成

--- app/src/main/java/com/deity/driftbottle/data/DriftBottleApplication.java
+++ app/src/main/java/com/deity/driftbottle/data/DriftBottleApplication.java
@@ -26,7 +26,7 @@ public class DriftBottleApplication extends Application {
     //TODO MicroMsg.SDK.WXApiImplV10: <init>, appId = null,为null是因为DriftBottleApplication没注册
     //各个平台的配置，建议放在全局Application或者程序入口
     {
-        PlatformConfig.setWeixin("wxdc1e388c3822c80b", "3baf1193c85774b3fd9d18447d76cab0");
+        PlatformConfig.setWeixin("wxaf363103f6a600fa", "18ca6f0c17b41b8e3b658f8a09c5e6d2");
         PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
     }
 }
